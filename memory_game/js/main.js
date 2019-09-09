c3b8b364d6da2ba3b553e3d942df017009c1c2b8
Mausumi Patra <someepatra@gmail.com>
2019-09-08 20:04:42 -0400

    memory_game/index.html

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
-console.log('Up and running!');
-var cardOne='Queen';
-var cardTwo='Queen';
-var cardThree='King';
-var cardFour='King';
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardTwo);
+
+ console.log('Up and running!');
+ var cards=['King','King','Queen','Queen'];
+ var cardsInPlay=[];
+
+ function checkForMatch()
+ {
+ 	if(cardsInPlay[0]===cardsInPlay[1])
+ 	{
+ 		console.log('You found a match!');
+ 	}
+ 		else
+ 		{
+ 			console.log('Sorry, try again.');
+ 	}
+ }
+ function flipcard(cardId)
+ {
+ 	console.log('User flipped ' + cards[cardId]);
+ 	cardsInPlay.push(cards[cardId]);
+ 	checkForMatch();
+ if (cardsInPlay.length===1)
+ {
+ 	
+ }
+}
+flipcard(0);
+flipcard(2);
