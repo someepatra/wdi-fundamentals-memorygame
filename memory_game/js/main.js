f61a890a73fb36689ff840524c21fb435a9f7bae
Mausumi Patra <someepatra@gmail.com>
2019-09-12 21:51:19 -0400

    memory_game/js/main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,51 @@
 
  console.log('Up and running!');
- var cards=['King','King','Queen','Queen'];
- var cardsInPlay=[];
+ //var cards=['King','King','Queen','Queen'];
+
+var cardsInPlay=[];
+var cards = [
+{
+rank:"queen",
+suits:"hearts",
+cardimage:"images/queen-of-hearts.png"
+},
+{
+rank:"queen",
+suits:"diamonds",
+cardimage:"images/queen-of-diamonds.png"
+},
+{
+rank:"king",
+suits:'hearts',
+cardimage:"images/king-of-hearts.png"
+},
+{
+rank:"king",
+suits:'diamonds',
+cardimage:"images/king-of-diamonds.png"
+}
+];
+ 
 
  function checkForMatch()
  {
  	if(cardsInPlay[0]===cardsInPlay[1])
  	{
- 		console.log('You found a match!');
+ 	console.log('You found a match!');
  	}
- 		else
- 		{
- 			console.log('Sorry, try again.');
+ 	else
+ 	{
+	alert('Sorry, try again.');
  	}
  }
+
  function flipcard(cardId)
  {
- 	console.log('User flipped ' + cards[cardId]);
- 	cardsInPlay.push(cards[cardId]);
- 	checkForMatch();
+ console.log('User flipped '+" " + cards[cardId].rank);
+ cardsInPlay.push(cards[cardId].rank);
+ console.log(cards[cardId].cardimage);
+ console.log(cards[cardId].suits);
+ checkForMatch();
  if (cardsInPlay.length===1)
  {
  	
